3a872cd8fdc22987cfe7a7323f77e7169611bd98
BaturinGS <37772861+BaturinGS@users.noreply.github.com>
2018-03-25 16:29:48 +0300

    Update JavaWarmUp001.java

--- JavaWarmUp001.java
+++ JavaWarmUp001.java
@@ -1,7 +1,5 @@
 // Сила тяжести на Луне примерно равна 17%, напишите программу, которая вычисляет ваш вес на Луне.
-
 import java.util.Scanner;
-
 public class JavaWarmUp001 {
     public static void main(String[] args){
         System.out.println("Введите свой вес в килограммах: ");
